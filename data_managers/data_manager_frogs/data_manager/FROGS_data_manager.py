9ea9291a373df390f56dbf51227187f796417f31
mariabernard <maria.bernard@inra.fr>
2021-08-18 17:31:40 +0200

    datamanager : remove HVL and phix loc and datatables, update version

--- data_managers/data_manager_frogs/data_manager/FROGS_data_manager.py
+++ data_managers/data_manager_frogs/data_manager/FROGS_data_manager.py
@@ -1,5 +1,28 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+#
+# Copyright (C) 2021 INRA
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+__author__ = 'David Christiany Migale Jouy en Josas / Maria Bernard - Sigenae Jouy en Josas'
+__copyright__ = 'Copyright (C) 2020 INRAE'
+__license__ = 'GNU General Public License'
+__version__ = '3.2.3'
+__email__ = 'frogs-support@inrae.fr'
+__status__ = 'prod'
 
 # import json
 import argparse
